c26f74345a37f15ef223d0cb053a45d8e4e8cb89
Hui Wang <hui.wang@canonical.com>
2014-12-03 10:42:39 +0800

    change to use global variables in the get_icon_name_for_volume

--- plugins/media-keys/gsd-media-keys-manager.c
+++ plugins/media-keys/gsd-media-keys-manager.c
@@ -257,6 +257,14 @@ static const char *volume_icons[] = {
         NULL
 };
 
+static const char *audio_volume_icons[] = {
+        "audio-volume-muted-symbolic",
+        "audio-volume-low-symbolic",
+        "audio-volume-medium-symbolic",
+        "audio-volume-high-symbolic",
+        NULL
+};
+
 static const char *mic_volume_icons[] = {
         "microphone-sensitivity-muted-symbolic",
         "microphone-sensitivity-low-symbolic",
@@ -617,20 +625,6 @@ get_icon_name_for_volume (gboolean is_mic,
                           gboolean muted,
                           int volume)
 {
-        static const char *icon_names[] = {
-                "audio-volume-muted-symbolic",
-                "audio-volume-low-symbolic",
-                "audio-volume-medium-symbolic",
-                "audio-volume-high-symbolic",
-                NULL
-        };
-        static const char *mic_icon_names[] = {
-                "microphone-sensitivity-muted-symbolic",
-                "microphone-sensitivity-low-symbolic",
-                "microphone-sensitivity-medium-symbolic",
-                "microphone-sensitivity-high-symbolic",
-                NULL
-        };
         int n;
 
         if (muted) {
@@ -646,9 +640,9 @@ get_icon_name_for_volume (gboolean is_mic,
         }
 
 	if (is_mic)
-		return mic_icon_names[n];
+		return mic_volume_icons[n];
 	else
-		return icon_names[n];
+		return audio_volume_icons[n];
 }
 
 static gboolean
