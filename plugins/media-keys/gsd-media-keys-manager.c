05847a30c7e043b37392f88fa54cd3359838a31d
Bastien Nocera <hadess@hadess.net>
2015-10-08 16:28:20 +0200

    media-keys: Fix compilation failure

--- plugins/media-keys/gsd-media-keys-manager.c
+++ plugins/media-keys/gsd-media-keys-manager.c
@@ -1358,6 +1358,19 @@ do_lock_screensaver (GsdMediaKeysManager *manager)
 }
 
 static void
+sound_theme_changed (GtkSettings         *settings,
+                     GParamSpec          *pspec,
+                     GsdMediaKeysManager *manager)
+{
+        char *theme_name;
+
+        g_object_get (G_OBJECT (manager->priv->gtksettings), "gtk-sound-theme-name", &theme_name, NULL);
+        if (theme_name)
+                ca_context_change_props (manager->priv->ca, CA_PROP_CANBERRA_XDG_THEME_NAME, theme_name, NULL);
+        g_free (theme_name);
+}
+
+static void
 ensure_canberra (GsdMediaKeysManager *manager)
 {
 	char *theme_name;
@@ -1614,19 +1627,6 @@ do_sound_action (GsdMediaKeysManager *manager,
                 osd_vol = 0;
 
         update_dialog (manager, stream, osd_vol, new_muted, sound_changed, quiet);
-}
-
-static void
-sound_theme_changed (GtkSettings         *settings,
-                     GParamSpec          *pspec,
-                     GsdMediaKeysManager *manager)
-{
-        char *theme_name;
-
-        g_object_get (G_OBJECT (manager->priv->gtksettings), "gtk-sound-theme-name", &theme_name, NULL);
-        if (theme_name)
-                ca_context_change_props (manager->priv->ca, CA_PROP_CANBERRA_XDG_THEME_NAME, theme_name, NULL);
-        g_free (theme_name);
 }
 
 static void
