9513788fca46c23881073fb2d1e83f39698584b4
Marco Trevisan (Treviño) <mail@3v1n0.net>
2018-04-13 16:21:21 -0400

    script: monitors-xml migration, make copies instead of moving files

--- scripts/01-usd-migration-monitors-xml.sh
+++ scripts/01-usd-migration-monitors-xml.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
+MONITORS_FILES="monitors.xml monitors.xml.backup"
 config_path="$HOME/.config"
 
 if [ -n "$XDG_CONFIG_HOME" ]; then
     config_path="$XDG_CONFIG_HOME";
 fi
 
-for monitor_file in monitors.xml monitors.xml.backup; do
+for monitor_file in $MONITORS_FILES; do
     old_monitors="$config_path/$monitor_file"
     new_monitors="$config_path/unity-$monitor_file"
 
     if [ -f "$old_monitors" ]; then
         if grep '<monitors[^>]*>' "$old_monitors" | grep -Fq 'version="1"'; then
-           mv -v "$old_monitors" "$new_monitors"
+           cp -v "$old_monitors" "$new_monitors"
         fi
     fi
 done
